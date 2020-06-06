2458fc959d0a77b80aa8c0321cd281578c112246
Nicholas Njihia <njihianicklas55@gmail.com>
2020-06-06 21:00:23 +0300

    Removed all the errors discovered by clippy

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,22 @@
-fn replace_duplicate(sentence: &str) -> Result<String> {
-    let text_vec = sentence.split_whitespace().collect::<Vec<_>>();
-    let concatenated_vec_words = text_vec.iter().dedup().fold(String::new(), |mut s, &text| {
+pub fn replace_duplicate(sentence: &str) -> String {
+    let mut text_vec = sentence.split_whitespace().collect::<Vec<&str>>();
+    text_vec.dedup();
+    let words = text_vec.iter().fold(String::new(), |mut s, &text| {
         s.push(' ');
-        s.push_str(w);
+        s.push_str(text);
         s
     });
-    concatenated_vec_words
+    String::from(words.trim())
 }
 
+#[cfg(test)]
 mod test {
     use super::*;
+    #[test]
     fn test_replace_duplicates() {
         let some_text_with_duplicates = "This is is something";
         assert_eq!(
             replace_duplicate(some_text_with_duplicates),
-            String::from("This is something")
-        );
+            "This is something".to_string());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use replace_adjacent_duplicates::replace_duplicate;
 fn main() {
     let text1 = "This is a duplicate of a duplicate duplicate duplicated!";
+    let text2 = "This is is something";
+
     println!("{}", replace_duplicate(text1));
+    println!("{}",replace_duplicate(text2));
 }
